95f8ef1b1f7f378f8cc12f68c2a2599df0230669
KaKaVip <haipq@ymeo.net>
2016-12-27 16:43:58 +0700

    Add new project

---
+++ app/src/main/java/asia/fivejuly/securepreferences/sample/MainActivity.java
@@ -0,0 +1,33 @@
+package asia.fivejuly.securepreferences.sample;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import asia.fivejuly.securepreferences.SecurePreferences;
+
+/**
+ * Created by haipq on 12/27/16.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        SharedPreferences shareConceal = new SecurePreferences.Builder(MainActivity.this)
+                .password("password")
+                .filename("settingsA")
+                .build();
+
+        shareConceal.edit().putString("hello","Xin chào bạn").apply();
+
+    }
+
+
+
+}
